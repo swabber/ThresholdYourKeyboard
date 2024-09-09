04e3753abcd0982b56820b27a721bc84ddf01e12
Andrew Kolesnyk <adr@simcorp.com>
2024-09-08 21:46:07 -0400

    Add Unittest checking posibility of ThresholdChecker can be extended with other types.

---
+++ ThresholdYourKeyboardTest/ThresholdIntervalCoverageChecker.cs
@@ -0,0 +1,53 @@
+﻿using ExtensionAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThresholdYourKeyboardTest {
+    /// <summary>
+    /// This class test different type ThresholdChecker scalability.
+    /// Data is sorted intervals.
+    /// Class checks if 2 intervals fully intersect at Threshold interval
+    /// </summary>
+    internal class ThresholdIntervalCoverageChecker : ThresholdChecker<List<(int, int)>> {
+        public override bool CheckThreshold(List<(int, int)> value1, List<(int, int)> value2) {
+            return FullyCovered(value1) && FullyCovered(value2);
+        }
+
+        public override bool SetThreshold(List<(int, int)> value) {
+            if (value == null) { return false; }
+            Threshold = value;
+            return true;
+        }
+
+        private bool FullyCovered(List<(int, int)> value) {
+            int j = 0;
+            foreach ((int, int) item in Threshold) {
+                int start = item.Item1;
+                int end = item.Item2;
+                while (j < value.Count) {
+                    if (value[j].Item2 <= start) {
+                        j++;
+                    } else if (start < value[j].Item2 && value[j].Item2 < end) {
+                        if (value[j].Item1 > start) {
+                            return false;
+                        } else {
+                            start = value[j].Item2;
+                            j++;
+                        }
+                    } else if (end < value[j].Item2) {
+                        if (value[j].Item1 <= start) {
+                            break;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ThresholdYourKeyboardTest/ThresholdIntervalCoverageCheckerTest.cs
@@ -0,0 +1,25 @@
+﻿using ExtensionAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThresholdYourKeyboardTest {
+    [TestClass]
+    public class ThresholdIntervalCoverageCheckerTest {
+
+        [TestMethod]
+        public void CheckThresholdTest() {
+            ThresholdChecker<List<(int, int)>> thresholdChecker = new ThresholdIntervalCoverageChecker();
+            thresholdChecker.SetThreshold(new List<(int, int)> { (1, 4), (5, 8) });
+
+            var list1 = new List<(int, int)> { (-3, 0), (1, 3), (3, 6), (6, 9), (10, 20)};
+            var list2 = new List<(int, int)> { (-3, 20) };
+            Assert.IsTrue(thresholdChecker.CheckThreshold(list1, list2));
+
+            list1 = new List<(int, int)> { (-3, 0), (1, 3), (3, 6), (7, 9), (10, 20) };
+            Assert.IsFalse(thresholdChecker.CheckThreshold(list1, list2));
+        }
+    }
+}
